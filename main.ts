f8d59ad99760d24d63c33811de3bc3d6896e463b
AmaanIqbal0011 <166907241+AmaanIqbal0011@users.noreply.github.com>
2024-04-28 21:42:42 +0500

    creating bmi calculator

---
+++ main.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let userHeight = await inquirer.prompt([
+    {
+        name : "height",
+        type : "number",
+        message : "Enter Your Height in Meters: "
+    },
+]);
+
+let userWeight = await inquirer.prompt([
+    {
+        name : "weight",
+        type : "number",
+        message : "Enter Your Weight in KiloGrams: "
+    },
+]);
+
+ 
+let heightSquare = userHeight.height * userHeight.height;
+let bmiCalculation = userWeight.weight / heightSquare;
+
+let bmi = (bmiCalculation.toFixed(1))
+
+console.log(`Your Body Mass Index is ${bmi}kg/m2`)
